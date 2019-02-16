fb2e3ef31a40dcd21bd71b9991267448fb6067de
sudhanshu150 <42195119+sudhanshu150@users.noreply.github.com>
2019-02-17 00:26:38 +0530

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,23 @@
+
+Input Format
+
+The first line contains the first integer, . The second line contains the second integer, .
+
+Output Format
+
+Print the two lines as described above.
+
+Sample Input 0
+
+4
+3
+Sample Output 0
+
+1
+1.33333333333
+
+program................
+a = int(input())
+b = int(input())
+print(int(a//b))
+print(float(a/b))
